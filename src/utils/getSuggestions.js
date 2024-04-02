157cee5864d1df484f85f26c91da656a04f33107
Emir <emirhansirkeci8@gmail.com>
2024-04-03 00:56:13 +0300

    Remove unused codes

--- src/utils/getSuggestions.js
+++ src/utils/getSuggestions.js
@@ -20,35 +20,5 @@ export const getSuggestions = (value, limit = 5) => {
     });
   });
 
-  // data.forEach((country, index) => {
-  //   country.states.forEach((state) => {
-  //     if (state.name.toLowerCase().startsWith(value.toLowerCase())) {
-  //       suggestions.push({
-  //         key: index,
-  //         found: state.name + "," + country.country,
-  //         country: country.country,
-  //         countryCode: country.countryCode,
-  //         state: state.name,
-  //         cities: state.cities,
-  //         html: state.name + " - " + country.country,
-  //       });
-  //     }
-
-  //     state.cities.forEach((city) => {
-  //       if (city.toLowerCase().startsWith(value.toLowerCase())) {
-  //         suggestions.push({
-  //           key: index,
-  //           found: city + "," + state.name + "," + country.country,
-  //           country: country.country,
-  //           countryCode: country.countryCode,
-  //           state: state.name,
-  //           city,
-  //           html: city + ", " + state.name + " - " + country.country,
-  //         });
-  //       }
-  //     });
-  //   });
-  // });
-
   return suggestions.splice(0, limit);
 };
