57b84a2bb8403c516e82a170539a3ef857520e11
Emir <emirhansirkeci8@gmail.com>
2024-04-27 14:55:27 +0300

    Add strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -7,22 +7,22 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    {/* <React.StrictMode> */}
-    <App />
-    <Toaster
-      position={"bottom-center"}
-      reverseOrder={false}
-      toastOptions={{
-        style: {
-          borderRadius: "10px",
-          background: "var(--gray-700)",
-          color: "var(--white)",
-          padding: "20px",
-        },
-        duration: 2000,
-      }}
-    />
-    {/* </React.StrictMode> */}
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+      <Toaster
+        position={"bottom-center"}
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            borderRadius: "10px",
+            background: "var(--gray-700)",
+            color: "var(--white)",
+            padding: "20px",
+          },
+          duration: 2000,
+        }}
+      />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
