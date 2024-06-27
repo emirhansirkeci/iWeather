82b2556deb00bc2b88166183449ad7a2d8683af3
Emir <emirhansirkeci8@gmail.com>
2024-06-27 23:16:41 +0300

    Change welcome message

--- src/pages/Home/WelcomeMessage.jsx
+++ src/pages/Home/WelcomeMessage.jsx
@@ -4,7 +4,7 @@ export default function WelcomeMessage() {
   return (
     <div className="welcome-message">
       <h4>
-        Welcome to <label>TypeWeather</label>
+        Welcome to <label>iWeather</label>
       </h4>
       <p className="text-sm">Choose a location to see the weather forecast</p>
     </div>
